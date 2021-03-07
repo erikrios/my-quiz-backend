f3ac1c48ef17c7fb603734d4684984541e7c68dc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-07 15:18:10 +0700

    Change the response message

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const config = require("config");
 const Response = require("../models/response");
 const router = express.Router();
+const message = config.get("message");
 
 router.get("/", (req, res) => {
-  res.send(new Response("OK", null, "Hello, World!"));
+  res.send(new Response("OK", null, message));
 });
 
 module.exports = router;
