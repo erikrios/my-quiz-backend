57d26aa4d4dfd535aa42a2c0079ff2669b2fb549
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-07 17:39:15 +0700

    Add categories routes

--- startup/routes.js
+++ startup/routes.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const home = require("../routes/home");
+const categories = require("../routes/categories");
 
 module.exports = (app) => {
   app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cors());
   app.use("/", home);
+  app.use("/api/categories", categories);
 };
