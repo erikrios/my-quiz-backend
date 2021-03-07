95553ad8d243f8f6eceb609b8978b87860e5f925
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-07 15:59:23 +0700

    Initialize the database

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const db = require("./db/models/index");
 
 require("./startup/routes")(app);
 require("./startup/prod")(app);
+
+connectToDatabase = async () => {
+  try {
+    await db.sequelize.sync();
+    console.log("Postgres connection has been established successfully.");
+  } catch (e) {
+    console.error(`Unable to connecto to the database: ${e}`);
+  }
+};
+
+connectToDatabase();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
